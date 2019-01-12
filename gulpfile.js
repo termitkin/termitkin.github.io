dca065272cc2677e52f2f0fac4afc0c47de68af4
termitkin <bigos911@gmail.com>
2019-01-12 21:34:53 +0300

    added .webp in img task

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('js', () =>
 )
 
 gulp.task('img', () =>
-  gulp.src(['./src/images/**/*.jpg', './src/images/**/*.jpeg', './src/images/**/*.png'])
+  gulp.src(['./src/images/**/*.jpg', './src/images/**/*.jpeg', './src/images/**/*.png', './src/images/**/*.webp'])
     .pipe(newer('./dist/images'))
     .pipe(gulp.dest('./dist/images'))
     .pipe(browserSync.reload({stream:true}))
@@ -52,5 +52,5 @@ gulp.task('browser-sync', () => {
     gulp.watch('src/css/*.css', gulp.series('css'));
     gulp.watch('src/index.html', gulp.series('html'));
     gulp.watch('src/js/**/*.js', gulp.series('js'));
-    gulp.watch(['./src/images/**/*.jpg', './src/images/**/*.jpeg', './src/images/**/*.png'], gulp.series('img'));
+    gulp.watch(['./src/images/**/*.jpg', './src/images/**/*.jpeg', './src/images/**/*.png', './src/images/**/*.webp'], gulp.series('img'));
 });
