aeb5f35bc1cb8e5dd616ac23d7e4673a399d3cd2
termitkin <bigos911@gmail.com>
2019-01-12 20:26:47 +0300

    Initial commit

---
+++ src/js/masonry-init.js
@@ -0,0 +1,10 @@
+/*
+ * Masonry init
+ */
+window.addEventListener('load', function() {
+  const elem = document.querySelector('.grid');
+  const msnry = new Masonry( elem, {
+    itemSelector: '.grid-item',
+    transitionDuration: '0.4s'
+  });
+});
